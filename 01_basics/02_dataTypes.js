7180714cb2317921a2f8a994dc1ef07746cc4242
VAISHNAVI SHARMA <2006sharmavaishnavi@gmail.com>
2026-08-08 14:47:47 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,17 @@
+"use strict"; // treat all js code as newer version
+//alert("HELLO")// we are using nodejs,not browser
+console.log(3+3)
+console.log("shreya")
+let name="shreya"
+let age = 18
+let isLoggedIn = false
+// number = 2^52
+// bigint
+//string = ""
+//boolean = true/false
+//null = standalone value
+// undefined = 
+console.log(typeof "shreya");
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
