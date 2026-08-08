34aa9d62aa0f6124c04d820f413444e62bb5c0b2
VAISHNAVI SHARMA <2006sharmavaishnavi@gmail.com>
2026-08-08 17:31:07 +0000

    OPERATIONS

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,4 +1,4 @@
-let score = true // check for 33 , "33abc" , null ,undefined ,true(boolean)
+/*let score = true // check for 33 , "33abc" , null ,undefined ,true(boolean)
 console.log(typeof score);
 console.log(typeof (score));
 let valueInNumber = Number(score)
@@ -13,3 +13,37 @@ let somenum =33
 let num = String(somenum);
 console.log(num);
 console.log(typeof num);
+*/
+// **************** Operations ***********************
+/*
+let value =3
+let negValue = -value
+console.log(negValue);
+console.log(2+2);
+console.log(2-2);
+console.log(2/2);
+console.log(2*2);
+console.log(2%2);
+console.log(2**3);
+*/
+/*
+let str1 = "hello"
+let str2 = " shreya"
+let str3 = str1+str2
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+*/
+console.log(true);//output --> true
+console.log(+true);//output --> 1  true+ will throw error not valid
+console.log(+"");// output --> 0
+
+let num1,num2,num3
+num1=num2=num3=2+2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
