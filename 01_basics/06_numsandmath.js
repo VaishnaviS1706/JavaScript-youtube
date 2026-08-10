a762721e43765fa35548116a7206fced6a0c2abc
VAISHNAVI SHARMA <2006sharmavaishnavi@gmail.com>
2026-08-10 17:18:52 +0000

    done

---
+++ 01_basics/06_numsandmath.js
@@ -0,0 +1,9 @@
+const score = 400//automatically type is number
+console.log(score)
+
+
+const balance = new Number(100)//explicitly type is 100
+console.log(balance);
+
+console.log(balance.toString().length);//type changed to string --> then we can use string function on it like concancate length etc.
+console.log(balance.toString())
